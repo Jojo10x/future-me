9f0dbb6f327f59e35066ac7f7dd9786b4835f725
Joel Kasisi <joelkasisi10@gmail.com>
2024-12-24 11:01:07 +0200

    new logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Goal } from "@/types/types";
 import { AddGoalButton } from "./AddGoalButton";
 import ProgressTracker from "./ProgressTracker";
+import logo from "../../public/logo--text----future-me.svg";
+import Image from "next/image";
 
 type HeaderProps = {
   goals: Goal[];
@@ -9,9 +11,16 @@ type HeaderProps = {
 
 export const Header: React.FC<HeaderProps> = ({ goals, onAddGoal }) => (
   <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
-    <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
+    {/* <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
       Future Me
-    </h1>
+    </h1> */}
+    <Image
+          width={100}
+          height={100}
+          src={logo}
+          alt="PixelTochka Logo"
+          className="ml-2"
+        />
     <ProgressTracker goals={goals} />
     <AddGoalButton onClick={onAddGoal} />
   </div>
