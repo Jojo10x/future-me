40f32c43f53d8c5cb6ad1f1fc31af24716fd48c6
Joel Kasisi <joelkasisi10@gmail.com>
2025-01-01 09:51:03 +0200

    implemented sorted goals

--- client/src/components/Goals/GoalList.tsx
+++ client/src/components/Goals/GoalList.tsx
@@ -1,5 +1,6 @@
-import GoalItem from '@/components/Goals/GoalItem';
-import { Goal } from '@/types/types';
+import React from "react";
+import GoalItem from "@/components/Goals/GoalItem";
+import { Goal } from "@/types/types";
 
 interface GoalListProps {
   goals: Goal[];
@@ -9,17 +10,34 @@ interface GoalListProps {
   onSubtaskComplete: (goalId: number, subtaskId: number) => void;
 }
 
-const GoalList: React.FC<GoalListProps> = ({ goals, onDelete, onComplete, onEdit, onSubtaskComplete }) => {
+const GoalList: React.FC<GoalListProps> = ({
+  goals,
+  onDelete,
+  onComplete,
+  onEdit,
+  onSubtaskComplete,
+}) => {
+  const sortedGoals = [...goals].sort((a, b) => {
+    if (a.is_completed === b.is_completed) {
+      return (
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+    }
+    return a.is_completed ? 1 : -1;
+  });
+
   return (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {goals.map((goal) => (
+      {sortedGoals.map((goal) => (
         <GoalItem
           key={goal.id}
           goal={goal}
           onDelete={() => onDelete(goal.id)}
           onComplete={() => onComplete(goal)}
           onEdit={() => onEdit(goal)}
-          onSubtaskComplete={(subtaskId) => onSubtaskComplete(goal.id, subtaskId)}
+          onSubtaskComplete={(subtaskId) =>
+            onSubtaskComplete(goal.id, subtaskId)
+          }
         />
       ))}
     </div>
